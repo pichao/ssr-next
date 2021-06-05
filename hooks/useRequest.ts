8bffa1e697dc5e6d85b64ab4bc56b8417dd9eb20
pitter <13821136099@163.com>
2021-06-06 03:05:56 +0800

    请求加上debounce

---
+++ hooks/useRequest.ts
@@ -0,0 +1,47 @@
+import { useState, useCallback, useMemo, useRef, useEffect } from 'react';
+import { debounce } from 'lodash-es';
+import axios from 'axios';
+export const useRequest = (params, callback = (data) => {}) => {
+    const [rParams, setRparams] = useState({
+        ...params,
+    });
+    const [isFetching, setIsFetching] = useState(true);
+    const [res, setRes] = useState(null);
+    const getData = useCallback(
+        debounce(async () => {
+            try {
+                const data = await axios({
+                    ...rParams,
+                });
+                console.log(data, 'kkkkkkkkkkkk');
+                setRes({
+                    status: data.status,
+                    data: data.data,
+                });
+                callback({
+                    status: data.status,
+                    data: data.data,
+                });
+            } catch (error) {
+                console.log(error.response, error.request, 'cuo');
+                if (error.response) {
+                    callback({
+                        status: error.response.status,
+                        data: null,
+                    });
+                    setRes({
+                        status: error.response.status,
+                        data: null,
+                    });
+                }
+            }
+            setIsFetching(false);
+        }, 1000),
+        [rParams],
+    );
+
+    useEffect(() => {
+        getData();
+    }, [rParams]);
+    return [isFetching, res, getData];
+};
